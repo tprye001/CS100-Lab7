7b135a3213c66b45422336970f6df21afa502369
Jonathan Alcaraz <jalca011@hammer.cs.ucr.edu>
2015-11-09 09:39:38 -0800

    made container.h and vectorContainer.h

---
+++ container.h
@@ -0,0 +1,17 @@
+#ifndef CONTAINER_H
+#define CONTAINER_H
+
+class Container {
+  private:
+    SortAlgortihm * s;
+  public:
+    virtual int at (int i) = 0;
+    virtual void swap(int i, int j) = 0;
+    virtual void insert (int element) = 0;
+    virtual void print () = 0;
+    virtual int size() = 0;
+    virtual void set_sort(SortAlgorithm* s) = 0;
+    virtual void sort () = 0;
+};
+
+#endif
