e14dd00ca8d39e93e69f137d956d3333c139cb48
Jonathan Alcaraz <jalca011@hammer.cs.ucr.edu>
2015-11-11 17:30:55 -0800

    mergsort

--- mergSort.h
+++ mergSort.h
@@ -13,33 +13,37 @@ class MergeSortAlgorithm : public virtual SortAlgorithm {
 };
 
 void merge(Container * c, int start, int end) {
-  int size = (start - end) + 1;
+  int size = (end - start) + 1;
   int mid = ((start + end) / 2) + 1;
 
-  std::vector<int> scratch(size);
+  std::vector<int> scratch(0);
 
   int i = start;
   int j = mid;
 
   for (int k = 0; k < size; k++) {
     if (c->at(i) < c->at(j)) {
-      scratch.at(k) = c->at(i);
+      scratch.push_back(c->at(i));
       i++;
     }
     else {
-      scratch.at(k) = c->at(j);
+      scratch.push_back(c->at(j));
       j++;
     }
     if(j > end) {
+      k++;
       while (i < mid) {
+        scratch.push_back(c->at(i));
+        i++;
         k++;
-        scratch.at(k) = c->at(i);
       }
     }
     else if (i >= mid) {
+      k++;
       while (j <= end) {
+        scratch.push_back(c->at(j));
+        j++;
         k++;
-        scratch.at(k) = c->at(i);
       }
     }
   }
